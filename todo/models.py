8552bf69fc7ee29a9593592accdd92aac59e5413
Jerzy Joniec <j.joniec5@gmail.com>
2026-05-21 12:47:56 +0200

    Finished todo app
    :wq

---
+++ todo/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Task(models.Model):
+    content = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=True, default=None)
+    is_done = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.content
